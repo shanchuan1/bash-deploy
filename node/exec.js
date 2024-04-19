670c8ef17e0b2762db0ba03f42cc62a4d09c0ad7
zhongqifeng <1121021693@qq.com>
2024-04-19 18:36:22 +0800

    feat: add node 初版

--- node/exec.js
+++ node/exec.js
@@ -1,18 +1,52 @@
 const { exec } = require("child_process");
+const { greenLog} = require('./terminalLog')
 
-const execProcess = async () => {
-  await exec(command, (error, stdout, stderr) => {
+global.DEFAULT_PACKAGE_MANAGER = "yarn";
+
+const packManager = {
+  yarn: {
+    INSTALL: (repo) => `cd ${repo.dest} && yarn`,
+    BUILD: (repo) => `cd ${repo.dest} && yarn build`,
+  },
+  npm: {
+    INSTALL: (repo) => `cd ${repo.dest} && npm run install`,
+    BUILD: (repo) => `cd ${repo.dest} && npm run build`,
+  },
+  pnpm: {
+    INSTALL: (repo) => `cd ${repo.dest} && pnpm install`,
+    BUILD: (repo) => `cd ${repo.dest} && pnpm run build`,
+  },
+};
+
+const execLog = (command, repo) => {
+  return (stdout) => {
+    command === "INSATLL" &&
+    greenLog(
+        `<<${repo.name}>> dependency has been installed and completed`
+      );
+    command === "BUILD" &&
+    greenLog(`<<${repo.name}>> has been packaged and built`);
+    console.log("stdout", stdout);
+  };
+};
+
+/* 执行shell脚本 */
+const execProcess = async (command, repo) => {
+  console.log("🚀 ~ execProcess ~ command:", command);
+  const bashCommand = packManager[global.DEFAULT_PACKAGE_MANAGER][command](repo);
+  console.log("🚀 ~ execProcess ~ bashCommand:", bashCommand);
+  await exec(bashCommand, (error, stdout, stderr) => {
     if (error) {
       console.error(`Failed to run 'yarn install' in ${repo.url}:`, error);
       return;
     }
-    console.log(`Yarn install output for ${repo.url}: ${stdout}`);
+    // console.log(`Yarn install output for ${repo.url}: ${stdout}`);
+    execLog(command, repo)(stdout);
     if (stderr) console.error(`Yarn install errors for ${repo.url}: ${stderr}`);
   });
 };
 
 
-
 module.exports = {
-    execProcess
-}
+  execProcess,
+};
--- node/repos.js
+++ node/repos.js
@@ -1,11 +1,23 @@
+const path = require("path");
+const proPath = path.resolve(__dirname, '../hospital')
+console.log('🚀 ~ proPath:', proPath)
+
+
 const repos = [
   {
-    url: "<repo1-url>",
-    dest: "/path/to/repo1",
+    url: "git@git.timevale.cn:public_health/esign-certification-h5.git",
+    dest: `${proPath}/esign-certification-h5`,
+    name: 'esign-certification-h5'
   },
   {
-    url: "<repo2-url>",
-    dest: "/path/to/repo2",
+    url: "git@git.timevale.cn:public_health/esign-hospital-ppm.git",
+    dest: `${proPath}/esign-hospital-ppm`,
+    name: 'esign-hospital-ppm'
+  },
+  {
+    url: "git@git.timevale.cn:public_health/esign-hospital-localsign.git",
+    dest: `${proPath}/esign-hospital-localsign`,
+    name: 'esign-hospital-localsign'
   },
 ];
 
